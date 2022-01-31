1ee903919691696e77fe1e3688c3605e8fc48674
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-31 21:58:20 +0300

    Update ExampleUnitTest.kt

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.example.diceroller
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,12 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun check_numbers() {
+        val dice = Dice(6)
+        val rollDice = dice.roll()
+        assertTrue("Dice produces 1 to 6", rollDice in 1..6)
     }
 }
